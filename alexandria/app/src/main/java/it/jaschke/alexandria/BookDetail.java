235eac6575dbb0fa8c396f7dae2fbb620d6ba93a
Gabriel Quiles-Perez <info@gabyquiles.com>
2015-12-03 23:59:28 -0500

    Prevent crash when orientation changed

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -71,7 +71,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         inflater.inflate(R.menu.book_detail, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
-        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        shareActionProvider = new ShareActionProvider(getActivity());
+        MenuItemCompat.setActionProvider(menuItem, shareActionProvider);
     }
 
     @Override
@@ -99,7 +100,10 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        //Add Null check, was crashing when orientation changed
+        if(shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
